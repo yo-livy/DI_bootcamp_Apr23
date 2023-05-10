fac97c379d66e3e332cfcc4db09d01028fd110ae
yo-livy <yo.livy7@gmail.com>
2023-05-10 17:50:13 +0300

    Day4 DailyChallenge

--- Week2/Day4/DailyChallenge/main.py
+++ Week2/Day4/DailyChallenge/main.py
@@ -1,3 +1,44 @@
+# Instructions
+#
+# Given a “Matrix” string:
+#
+#     7i3
+#     Tsi
+#     h%x
+#     i #
+#     sM
+#     $a
+#     #t%
+#     ^r!
+#
+#
+# The matrix is a grid of strings (alphanumeric characters and spaces) with a hidden message in it.
+# A grid means that you could potentially break it into rows and columns, like here:
+#
+# 7	i	3
+# T	s	i
+# h	%	x
+# i		#
+# s	M
+# $	a
+# #	t	%
+# ^	r	!
+#
+#
+# To decrypt the matrix, Neo reads each column from top to bottom, starting from the leftmost column, selecting only the alpha characters and connecting them. Then he replaces every group of symbols between two alpha characters by a space.
+#
+# Using his technique, try to decode this matrix.
+#
+# Hints:
+# Use
+# ● lists for storing data
+# ● Loops for going through the data
+# ● if/else statements to check the data
+# ● String for the output of the secret message
+#
+# Hint (if needed) : Look at the remote learning “Matrix” videos
+
+
 a = "7i3"
 b = "Tsi"
 c = "h%x"
