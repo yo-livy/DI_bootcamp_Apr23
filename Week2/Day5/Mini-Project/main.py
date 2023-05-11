32f29b7e633efe440ab654020ce287663354cb4f
yo-livy <yo.livy7@gmail.com>
2023-05-11 19:11:41 +0300

    Day4 DailyChallenge

--- Week2/Day5/Mini-Project/main.py
+++ Week2/Day5/Mini-Project/main.py
@@ -1,36 +1,121 @@
-def display_board():
-    print("TIC TAC TOE")
-    for i in range(7):
+# Main variables (global)
+board = [[" "," "," "],
+         [" "," "," "],
+         [" "," "," "]]
+currentPlayer = "X"
+winner = None
+gameRunning = True
+
+# --------------------------------------------------
+# Board printing func
+def printBoard(board):
+    print("\nTIC TAC TOE")
+    for i in range(9):
         row = ""
-        for j in range(28):
-            if i==0 or i == 6 or j == 0 or j == 27:
+        for j in range(36):
+            if i==0 or i == 8 or j == 0 or j == 35:
                 row += "*"
-            elif (i==2 or i==4) and j in range(5,23):
-                row += "-"
-            elif j == 10 or j == 17:
+            elif j == 12 or j == 22:
                 row += "|"
+            elif (i==3 or i==5) and j in range(4,31):
+                row += "-"
+            elif (i==2 and j==8):
+                row += board[0][0]
+            elif (i==2 and j==17):
+                row += board[0][1]
+            elif (i==2 and j==26):
+                row += board[0][2]
+            elif (i==4 and j==8):
+                row += board[1][0]
+            elif (i==4 and j==17):
+                row += board[1][1]
+            elif (i==4 and j==26):
+                row += board[1][2]
+            elif (i==6 and j==8):
+                row += board[2][0]
+            elif (i==6 and j==17):
+                row += board[2][1]
+            elif (i==6 and j==26):
+                row += board[2][2]
             else:
                 row += " "
         print(row)
 
-display_board()
+#Player input func
+def playerInput(board):
+    while True:
+        print(f"\nPlayer {currentPlayer}'s turn ... ")
+        row = int(input("\nEnter a row (1 - 3): "))
+        col = int(input("Enter a column (1 - 3): "))
+        if (1 <= row <= 3) and (1 <= col <= 3) and board[row-1][col-1] == " ":
+            board[row-1][col-1] = currentPlayer
+            break
+        else:
+            print("Sorry, the spot is not empty")
 
-def player_input(player):
-    print(f"Player {player}'s turn...")
-    row = ''
-    column = ''
-    while row not in [1, 2, 3]:
-        row = int(input("Enter row (1, 2 or 3): "))
-    while column not in [1, 2, 3] :
-        column = int(input("Enter column (1, 2 or 3: "))
-    position = (row, column)
-    print(position)
-    return position
-player_input("x")
+#check for winning combinations (3 func: horizontal, vertical, diagonal)
+def checkHorizontle(board):
+    global winner
+    if board[0][0] == board[0][1] == board[0][2] and board[0][0] != " ":
+        winner = board[0][0]
+        return True
+    elif board[1][0] == board[1][1] == board[1][2] and board[1][0] != " ":
+        winner = board[1][0]
+        return True
+    elif board[2][0] == board[2][1] == board[2][2] and board[2][0] != " ":
+        winner = board[2][0]
+        return True
+def checkVertical(board):
+    global winner
+    if board[0][0] == board[1][0] == board[2][0] and board[0][0] != " ":
+        winner = board[0][0]
+        return True
+    elif board[0][1] == board[1][1] == board[2][1] and board[0][1] != " ":
+        winner = board[0][1]
+        return True
+    elif board[0][2] == board[1][2] == board[2][2] and board[0][2] != " ":
+        winner = board[0][2]
+        return True
+def checkDiagonal(board):
+    global winner
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != " ":
+        winner = board[0][0]
+        return True
+    elif board[2][0] == board[1][1] == board[0][2] and board[2][0] != " ":
+        winner = board[2][0]
+        return True
+
+#Win checker func
+def checkWin():
+    global gameRunning
+    if checkHorizontle(board) or checkVertical(board) or checkDiagonal(board):
+        print(f"\nThe winner is {winner}!")
+        printBoard(board)
+        gameRunning = False
+
+#Tie checker func
+def checkTie(board):
+    global gameRunning
+    if "*" not in board[0] and " " not in board[1] and " " not in board[2]:
+        print("It's a tie!")
+        gameRunning = False
+
+#Player switcher func
+def switchPlayer():
+    global currentPlayer
+    if currentPlayer == "X":
+        currentPlayer = "O"
+    else:
+        currentPlayer = "X"
 
 
-
-
-
+# main function (gather all functions)
+print("Weclome to TIC TAC TOE!")
+while gameRunning:
+   printBoard(board)
+   playerInput(board)
+   checkWin()
+   checkTie(board)
+   switchPlayer()
 
 
