115bae4cd5966155204e44866edf3aef71a4eb9b
yo-livy <yo.livy7@gmail.com>
2023-05-16 20:24:46 +0300

    Commit

--- Week3/Day3/ExercisesXP/main_plus.py
+++ Week3/Day3/ExercisesXP/main_plus.py
@@ -94,6 +94,38 @@ def today_date():
 
 print(today_date())
 
+# Exercise 8 : How Old Are You On Jupiter?
+#
+# Instructions
+#
+# Given an age in seconds, calculate how old someone would be on:
+# Earth: orbital period 365.25 Earth days, or 31557600 seconds
+# Mercury: orbital period 0.2408467 Earth years
+# Venus: orbital period 0.61519726 Earth years
+# Mars: orbital period 1.8808158 Earth years
+# Jupiter: orbital period 11.862615 Earth years
+# Saturn: orbital period 29.447498 Earth years
+# Uranus: orbital period 84.016846 Earth years
+# Neptune: orbital period 164.79132 Earth years
+# So if you are told someone is 1,000,000,000 seconds old, the function should output that they are 31.69 Earth-years old.
+
+def how_old(seconds):
+    earth = seconds / 31557600
+    other_planet = {
+        'Earth' : earth,
+        'Mercury' : 0.2408467,
+        'Venus' : 0.61519726,
+        'Mars' : 1.8808158,
+        'Jupiter' : 11.862615,
+        'Saturn' : 29.447498,
+        'Uranus' : 84.016846,
+        'Neptune' : 164.79132
+    }
+    for key, value in other_planet.items():
+        print(f"{key}: {value * earth} earth years")
+how_old(1000000000)
+
+
 
 
 
