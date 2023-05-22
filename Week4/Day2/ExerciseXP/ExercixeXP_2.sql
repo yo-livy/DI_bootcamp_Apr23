a019cc225f8766cd959ab122e2858189d8cff5e0
yo-livy <yo.livy7@gmail.com>
2023-05-22 16:19:13 +0300

    Commit

--- Week4/Day2/ExerciseXP/ExercixeXP_2.sql
+++ Week4/Day2/ExerciseXP/ExercixeXP_2.sql
@@ -63,17 +63,52 @@
 
 -- Write a query which will find the 10 cheapest movies.
 
-SELECT * FROM 
+-- SELECT title, rental_rate FROM film
+-- ORDER BY rental_rate ASC LIMIT 10
 
 
 
--- Not satisfied with the results. Write a query which will find the next 10 cheapest movies. 
+-- Not satisfied with the results. Write a query which will find the next 10 cheapest movies.
+
+-- SELECT title, rental_rate FROM film
+-- ORDER BY rental_rate ASC LIMIT 10 OFFSET 10
+
+
+
 -- Bonus: Try to not use LIMIT.
+
+-- SELECT title, rental_rate FROM film
+-- WHERE rental_rate = (SELECT MIN(rental_rate) FROM film)
+
+
 
 -- Write a query which will join the data in the customer table and the payment table. You want to get the first name and last name from the curstomer table, as well as the amount and the date of every payment made by a customer, ordered by their id (from 1 to…).
 
+-- SELECT first_name, last_name, amount, payment_date, customer.customer_id FROM customer
+-- INNER JOIN payment ON customer.customer_id = payment.customer_id
+-- ORDER BY customer.customer_id ASC
+
+
+
 -- You need to check your inventory. Write a query to get all the movies which are not in inventory.
+
+-- SELECT * FROM film
+-- WHERE film_id NOT IN (SELECT DISTINCT film_id FROM inventory)
+
+
 
 -- Write a query to find which city is in which country.
 
+-- SELECT city, country.country FROM city
+-- LEFT JOIN country ON city.country_id = country.country_id
+
+
+
 -- Bonus You want to be able to see how your sellers have been doing? Write a query to get the customer’s id, names (first and last), the amount and the date of payment ordered by the id of the staff member who sold them the dvd.
+
+SELECT customer.customer_id, customer.first_name, customer.last_name, amount, DATE(payment_date) AS date, staff_id FROM payment
+FULL JOIN customer ON customer.customer_id = payment.customer_id
+ORDER BY staff_id ASC
+
+
+
