5b63574dae808239718d96858530f37eb3248a6d
yo-livy <yo.livy7@gmail.com>
2023-05-29 07:50:01 +0300

    Issue with id

--- Week5/Day1/ExercisesXP/animal-info/animals/animals/urls.py
+++ Week5/Day1/ExercisesXP/animal-info/animals/animals/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from info.views import family, animal
+from info.views import family, animals
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('family/', family),
-    path('animal/', animal)
+    path('animal/<id>', animals)
 ]
--- Week5/Day1/ExercisesXP/animal-info/animals/info/views.py
+++ Week5/Day1/ExercisesXP/animal-info/animals/info/views.py
@@ -10,11 +10,15 @@ def family(request):
     context = {}
     return render(request, 'family.html', context)
 
-def animal(request):
-    file_path = '/Users/yo.livy/Desktop/Week5/Day1/ExercisesXP/animal-info/animals/info/animals.json'
-    with open(file_path, 'r') as file:
+def animals(request, id):
+    with open('/Users/yo.livy/Desktop/Week5/Day1/ExercisesXP/animal-info/animals/info/animals.json') as file:
         data = json.load(file)
-    context = {
-        'animals': data['animals']
-    }
+    
+    for animal in data['animals']:
+        if animal['id'] == int(id):
+            context = {"Name": animal['name'],
+                           "Legs": animal['legs'],
+                            "Weight": animal['weight'],
+                            "Height": animal['height'],
+                             "Speed": animal['speed']}
     return render(request, 'animal.html', context)
