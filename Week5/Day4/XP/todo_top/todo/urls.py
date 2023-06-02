34f6868f8ce9f58f9d7889bcf34838753b241a79
yo-livy <yo.livy7@gmail.com>
2023-06-02 11:15:27 +0300

    Update on 'Done' button in w4d4 xp

--- Week5/Day4/XP/todo_top/todo/urls.py
+++ Week5/Day4/XP/todo_top/todo/urls.py
@@ -5,5 +5,6 @@ from todo_app.views import todo, display_todos, todo_done
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todo/', todo),
-    path('display-todos/', display_todos),
+    path('display-todos/', display_todos, name='display_todos'),
+    path('todo-done/<int:todo_id>/', todo_done, name='todo_done')
 ]
