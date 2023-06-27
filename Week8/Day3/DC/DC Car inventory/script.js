3ed1bfb7362343d80855851e0e77becbd71839f7
yo-livy <yo.livy7@gmail.com>
2023-06-27 14:52:59 +0300

    w8d3 dc car inventory

---
+++ Week8/Day3/DC/DC Car inventory/script.js
@@ -0,0 +1,25 @@
+const inventory = [
+    { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+    { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+    { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+    { id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+    { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+  ];
+
+const getCarHonda = (carInventory) => {
+    const honda = carInventory.find((element) => element['car_make'] === 'Honda');
+    return `This is a ${honda['car_make']} ${honda['car_model']} from ${honda['car_year']}.`
+}
+
+const honda = getCarHonda(inventory);
+console.log(honda);
+
+const sortCarInventoryByYear = (carInventory) => {
+    return carInventory.sort((a, b) => a.car_year - b.car_year);
+}
+
+const sortedCars = sortCarInventoryByYear(inventory);
+console.log(sortedCars);
+
+
+
