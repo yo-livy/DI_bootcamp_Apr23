61a9abb105790f37bb59f73a761752c60f6d9924
yo-livy <yo.livy7@gmail.com>
2023-06-29 16:16:58 +0300

    w8d5 dc done

---
+++ Week8/Day5/DC/script.js
@@ -0,0 +1,12 @@
+const anagram = (a, b) => {
+    const aArr = a.toLowerCase().replace(/\s/g, '').split('');
+    const bArr = b.toLowerCase().replace(/\s/g, '').split('');
+    console.log(aArr);
+    console.log(bArr);
+    if(aArr.length === bArr.length && aArr.every((element) => bArr.includes(element))) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(anagram('hello', 'ehllo'));
