95586baf597183296c3f346d755cf33e79f6e10f
yo-livy <yo.livy7@gmail.com>
2023-07-09 14:58:42 +0300

    w10d1 xp done

---
+++ Week10/Day1/XP/npm-mode/main.js
@@ -0,0 +1,14 @@
+const a = 5;
+const b = 10;
+
+console.log(`Hello you are ${a + b}`);
+console.log('Hello, how are you!');
+
+const d = 5;
+const f = 10;
+let x;
+const c = 6;
+x = d + f;
+
+console.log(x);
+console.log(x + c);
