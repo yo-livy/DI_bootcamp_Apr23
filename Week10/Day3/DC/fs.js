327ce2ee6970ace98677da0f947627f38431ac45
yo-livy <yo.livy7@gmail.com>
2023-07-11 20:11:50 +0300

    w10d3 dc done

---
+++ Week10/Day3/DC/fs.js
@@ -0,0 +1,46 @@
+const fs = require('fs')
+
+fs.readFile('RightLeft.txt','utf-8', (err, data) => {
+    if(err) return console.log(err);
+    let position = 0;
+    const arrRL = data.split('');
+    console.log(arrRL);
+    arrRL.forEach((element) => {
+        if(element==='>'){
+            position++;
+        }else{
+            position--;
+        }
+    })
+    if (position > 0){
+        console.log(`${position} steps to the right`)
+    }else {
+        (`${position} steps to the left`)
+    }
+});
+
+
+
+
+
+fs.readFile('RightLeft.txt','utf-8', (err, data) => {
+    if(err) return console.log(err);
+    let position = 0;
+    let totalSteps = 0
+    const arrRL = data.split('');
+    console.log(arrRL);
+    for(let i = 0; i < data.length; i++){
+        if(data[i] === '>'){
+            position++;
+        }else if(data[i] === '<'){
+            position--;
+        }
+        totalSteps++;
+        if(position === -1){
+            break;
+        }
+    }
+    console.log(totalSteps);
+});
+
+    
